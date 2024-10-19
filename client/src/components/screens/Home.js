636e92771dbdda3ac547d50fe42c36f820c80d00
DineshKuddana <dineshkuddana@gmail.com>
2024-10-19 18:15:23 +0530

    updated backend

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,115 +1,151 @@
-import React,{useState,useEffect,useContext} from 'react'
-import swal from 'sweetalert'
-import {UserContext} from '../../App'
+import React, { useState, useEffect, useContext } from "react";
+import swal from "sweetalert";
+import { UserContext } from "../../App";
 import Profile from "./Profile";
 
-const Home  = ()=>{
-    const [data,setData] = useState([])
-    const {state} = useContext(UserContext)
-    useEffect(()=>{
-       fetch('https://online-voting-system-backend-p62t.onrender.com/allpost',{
-           headers:{
-               "Authorization":"Bearer "+localStorage.getItem("jwt")
-           }
-       }).then(res=>res.json())
-       .then(result=>{
-           console.log(result)
-           if (result && result.posts && result.posts.length > 0) {
-            setData(result.posts); // Assuming result.posts is the array of posts
-          } else {
-            setData([]);
-          }
-       })
-       .catch(err => console.error('Error fetching posts:', err));
-    },[])
+const Home = () => {
+  const [data, setData] = useState([]);
+  const { state } = useContext(UserContext);
+  useEffect(() => {
+    fetch("https://online-voting-backend-y9ri.onrender.com/allpost", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        if (result && result.posts && result.posts.length > 0) {
+          setData(result.posts); // Assuming result.posts is the array of posts
+        } else {
+          setData([]);
+        }
+      })
+      .catch((err) => console.error("Error fetching posts:", err));
+  }, []);
 
-    const votePlayer = (id,userId)=>{
-        swal({
-            title: "Are you sure?",
-            text: "Once selected, you will not be able to reselect the options!",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
+  const votePlayer = (id, userId) => {
+    swal({
+      title: "Are you sure?",
+      text: "Once selected, you will not be able to reselect the options!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        fetch("https://online-voting-backend-y9ri.onrender.com/vote", {
+          method: "put",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("jwt"),
+          },
+          body: JSON.stringify({
+            postId: id,
+            userId,
+          }),
+        })
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result);
+            const newData = data.map((item) => {
+              if (item._id === result._id) {
+                return result;
+              } else {
+                return item;
+              }
+            });
+            setData(newData);
           })
-          .then((willDelete) => {
-            if (willDelete) {
-                fetch('https://online-voting-system-backend-p62t.onrender.com/vote',{
-                    method:"put",
-                    headers:{
-                        "Content-Type":"application/json",
-                        "Authorization":"Bearer "+localStorage.getItem("jwt")
-                    },
-                    body:JSON.stringify({
-                        postId:id,
-                        userId
-                    })
-                }).then(res=>res.json())
-                .then(result=>{
-                console.log(result)
-                  const newData = data.map(item=>{
-                      if(item._id===result._id){
-                          return result
-                      }else{
-                          return item
-                      }
-                  })
-                  setData(newData)
-                }).catch(err=>{
-                    console.error('Error voting:', err)
-                })  
-              swal("You successfully has given your vote!", {
-                icon: "success",
-              });
-            } else {
-              swal("Again ,select your option!");
-            }
+          .catch((err) => {
+            console.error("Error voting:", err);
           });
-         
-    }
-   
-    
-    let isVote;
-    let user = localStorage.getItem("user");
-    if (user) {
-        user = JSON.parse(user);
-        isVote = user._id;
-    }
+        swal("You successfully has given your vote!", {
+          icon: "success",
+        });
+      } else {
+        swal("Again ,select your option!");
+      }
+    });
+  };
 
-    const userId = user ? user._id : null;
+  let isVote;
+  let user = localStorage.getItem("user");
+  if (user) {
+    user = JSON.parse(user);
+    isVote = user._id;
+  }
 
-    const userIdExist = data.map(item => item.votes.filter(vote => vote));
+  const userId = user ? user._id : null;
 
-    let newVlaue = false;
-    for(let key of userIdExist) {
-      if(key.length > 0 ){
-         
-         for(let key2 of key){
-             if(key2 === isVote){
-                newVlaue = true ;
-             }
+  const userIdExist = data.map((item) => item.votes.filter((vote) => vote));
+
+  let newVlaue = false;
+  for (let key of userIdExist) {
+    if (key.length > 0) {
+      for (let key2 of key) {
+        if (key2 === isVote) {
+          newVlaue = true;
         }
       }
     }
+  }
 
-return (
+  return (
     <div>
       <div className="col-md-8" style={{ margin: "auto", paddingTop: "5%" }}>
-        <div className='col-md-3' style={{ float: "left", border: "1px solid green"}}>
+        <div
+          className="col-md-3"
+          style={{ float: "left", border: "1px solid green" }}
+        >
           <Profile />
         </div>
-        <div className='col-md-8' style={{ float: "right", backgroundColor: "white", padding: "10px" }}>
+        <div
+          className="col-md-8"
+          style={{ float: "right", backgroundColor: "white", padding: "10px" }}
+        >
           {data.length > 0 ? (
             data.map((item, index) => (
-              <div key={item._id} style={{ marginBottom: "10px", backgroundColor: "#f2f4f6", height: "100px", borderRadius: "15px", padding: "25px" }}>
-                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                  <h6><b>{index + 1}</b></h6>
-                  <h6><b>{item.title.split(" ")[0]}</b></h6>
-                  <img src={item.photo} style={{ height: "75px", width: "75px", margin: "-15px"}} alt="post" />
+              <div
+                key={item._id}
+                style={{
+                  marginBottom: "10px",
+                  backgroundColor: "#f2f4f6",
+                  height: "100px",
+                  borderRadius: "15px",
+                  padding: "25px",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <h6>
+                    <b>{index + 1}</b>
+                  </h6>
+                  <h6>
+                    <b>{item.title.split(" ")[0]}</b>
+                  </h6>
+                  <img
+                    src={item.photo}
+                    style={{ height: "75px", width: "75px", margin: "-15px" }}
+                    alt="post"
+                  />
                   <div className="card-content">
                     {!item.votes.includes(state._id) && !newVlaue ? (
-                      <button className="btn btn-success" onClick={() => votePlayer(item._id, userId)}>Vote</button>
+                      <button
+                        className="btn btn-success"
+                        onClick={() => votePlayer(item._id, userId)}
+                      >
+                        Vote
+                      </button>
                     ) : (
-                      <button className="btn disabled" style={{ marginLeft: "47px", background: "red" }}>
+                      <button
+                        className="btn disabled"
+                        style={{ marginLeft: "47px", background: "red" }}
+                      >
                         Already Voted
                       </button>
                     )}
@@ -126,6 +162,4 @@ return (
   );
 };
 
-
-
-export default Home
+export default Home;
--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,83 +1,120 @@
-import React, { useState, useContext } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { userType } from '../utils';
-import { UserContext } from '../../App';
+import React, { useState, useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { userType } from "../utils";
+import { UserContext } from "../../App";
 import Posts from "./Posts";
-import swal from 'sweetalert';
+import swal from "sweetalert";
 import Image from "../../Image/Capture.JPG";
 
 const SignIn = () => {
-    const { state, dispatch } = useContext(UserContext);
-    const history = useHistory();
-    const [password, setPasword] = useState("");
-    const [email, setEmail] = useState("");
-    // const [age, setAge] = useState("");
-    const [userItem, setuserItem] = useState('Voter');
+  const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
+  const [password, setPasword] = useState("");
+  const [email, setEmail] = useState("");
+  // const [age, setAge] = useState("");
+  const [userItem, setuserItem] = useState("Voter");
 
-    const PostData = () => {
-        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
-            swal("Error", "Invalid email", "error");
-            return;
-        }
-        fetch("https://online-voting-system-backend-p62t.onrender.com/signin", {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                password,
-                email,
-                // age,
-                userItem
-            })
-        }).then(res => res.json())
-            .then(data => {
-                if (data.error) {
-                    swal("Error", data.error, "error");
-                } else {
-                    localStorage.setItem("jwt", data.token);
-                    localStorage.setItem("user", JSON.stringify(data.user));
-                    dispatch({ type: "USER", payload: data.user });
-                    history.push('/');
-                }
-            }).catch(err => {
-                console.log(err);
-            });
+  const PostData = () => {
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      swal("Error", "Invalid email", "error");
+      return;
     }
+    fetch("https://online-voting-backend-y9ri.onrender.com/signin", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        password,
+        email,
+        // age,
+        userItem,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          swal("Error", data.error, "error");
+        } else {
+          localStorage.setItem("jwt", data.token);
+          localStorage.setItem("user", JSON.stringify(data.user));
+          dispatch({ type: "USER", payload: data.user });
+          history.push("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <div className="mycard" style={{ display: "flex", justifyContent: "space-between", paddingTop: "5%" }}>
-            <p><b style={{ marginLeft: "5%", fontSize: "24px" }}></b></p>
-            <div className="clo-6" style={{ flex: 1, marginRight: "2%", marginLeft: "10%" }}>
-                <Posts />
+  return (
+    <div
+      className="mycard"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        paddingTop: "5%",
+      }}
+    >
+      <p>
+        <b style={{ marginLeft: "5%", fontSize: "24px" }}></b>
+      </p>
+      <div
+        className="clo-6"
+        style={{ flex: 1, marginRight: "2%", marginLeft: "10%" }}
+      >
+        <Posts />
+      </div>
+      <div className="col-6" style={{ flex: 1, maxWidth: "400px" }}>
+        <div
+          className="card"
+          style={{
+            paddingLeft: "40px",
+            paddingRight: "40px",
+            paddingTop: "20px",
+            border: "1px solid black",
+            marginRight: "30%",
+          }}
+        >
+          <img
+            src={Image}
+            alt="image"
+            style={{
+              height: "90px",
+              width: "90px",
+              borderRadius: "50%",
+              alignSelf: "center",
+              marginBottom: "15px",
+            }}
+          />
+          <div>
+            <div className="col">
+              <input
+                type="text"
+                placeholder="email"
+                value={email}
+                className="form-control"
+                style={{ height: "50px", width: "100%", marginBottom: "10px" }}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <br />
             </div>
-            <div className="col-6" style={{ flex: 1, maxWidth: "400px" }}>
-                <div className="card" style={{ paddingLeft: "40px", paddingRight: "40px", paddingTop: "20px", border: "1px solid black", marginRight: "30%" }}>
-                    <img src={Image} alt="image" style={{ height: "90px", width: "90px", borderRadius: "50%", alignSelf: "center", marginBottom: "15px" }} />
-                    <div>
-                        <div className="col">
-                            <input
-                                type="text"
-                                placeholder="email"
-                                value={email}
-                                className='form-control'
-                                style={{ height: "50px", width: "100%", marginBottom: "10px" }}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                            <br />
-                        </div>
-                        <div className="col">
-                            <input
-                                type="password"
-                                placeholder="password"
-                                value={password}
-                                style={{ height: "50px", width: "100%", marginBottom: "10px" }}
-                                onChange={(e) => setPasword(e.target.value)}
-                                className='form-control'
-                            />
-                        </div>
-                        <br />
-                        {/* <div className="col">
+            <div className="col">
+              <input
+                type="password"
+                placeholder="password"
+                value={password}
+                style={{ height: "50px", width: "100%", marginBottom: "10px" }}
+                onChange={(e) => setPasword(e.target.value)}
+                className="form-control"
+              />
+            </div>
+            <br />
+            {/* <div className="col">
                             <input
                                 type="number"
                                 placeholder="age"
@@ -88,32 +125,46 @@ const SignIn = () => {
                             />
                         </div>
                         <br /> */}
-                        <div className="col">
-                            <select className="form-select" value={userItem} onChange={(e) => setuserItem(e.target.value)}
-                                style={{ height: "50px", width: "100%", marginBottom: "20px" }}
-                            >
-                                {userType.map((item) => (
-                                    <option value={item} key={item} >
-                                        {" "}
-                                        {item}{" "}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
-                    </div>
-                    <button className="btn btn-success mt-4"
-                        style={{ height: "50px", width: "100%" }}
-                        onClick={() => PostData()}
-                    >
-                        Login
-                    </button>
-                    <p style={{ fontFamily: "Raleway", textAlign: "center", fontSize: "17px", marginTop: "20px" }}>
-                        Not Register ? <Link to="/signup" style={{ color: "grey", fontWeight: "600" }}>Register here</Link>
-                    </p>
-                </div>
+            <div className="col">
+              <select
+                className="form-select"
+                value={userItem}
+                onChange={(e) => setuserItem(e.target.value)}
+                style={{ height: "50px", width: "100%", marginBottom: "20px" }}
+              >
+                {userType.map((item) => (
+                  <option value={item} key={item}>
+                    {" "}
+                    {item}{" "}
+                  </option>
+                ))}
+              </select>
             </div>
+          </div>
+          <button
+            className="btn btn-success mt-4"
+            style={{ height: "50px", width: "100%" }}
+            onClick={() => PostData()}
+          >
+            Login
+          </button>
+          <p
+            style={{
+              fontFamily: "Raleway",
+              textAlign: "center",
+              fontSize: "17px",
+              marginTop: "20px",
+            }}
+          >
+            Not Register ?{" "}
+            <Link to="/signup" style={{ color: "grey", fontWeight: "600" }}>
+              Register here
+            </Link>
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default SignIn;
